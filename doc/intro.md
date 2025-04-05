50686723bfe578288c0c09c9463a549961b18061
nenadgolubovic <nenad.golubovic999@gmail.com>
2025-04-05 20:05:02 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to catan-predictor
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/catan_predictor/core_test.clj
@@ -0,0 +1,7 @@
+(ns catan-predictor.core-test
+  (:require [clojure.test :refer :all]
+            [catan-predictor.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
