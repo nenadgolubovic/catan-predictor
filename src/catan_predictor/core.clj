a7ad5c0ce667e0662c0ea69541fc640a35a2b372
nenadgolubovic <nenad.golubovic999@gmail.com>
2025-04-06 13:46:54 +0200

    Add cards, recourses , build-type and areas

--- src/catan_predictor/core.clj
+++ src/catan_predictor/core.clj
@@ -1,6 +1,17 @@
 (ns catan-predictor.core
   (:gen-class))
 
+(def recourses #{"wool" "brick" "wood" "ore" "grain"})
+(def development-card #{"knight" "victory-point" "road-building" "monopoly" "year-of-plenty"})
+(def build-type #{"village" "road" "city"})
+(def areas #{"forest" "pastures" "fields" "hills" "mountains"})
+
+
+(defn value-card
+  []
+  )
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
